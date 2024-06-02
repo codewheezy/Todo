7b3b2b14ba1da4e6c54f65435c611a6f3ad56c81
codewheezy <uchennaibe95@gmail.com>
2024-06-02 07:35:33 +0100

    added new change to the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Model, TodoItem } from './model';
+import { NgFor, NgSwitchCase, NgSwitch, NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, NgSwitchCase, NgSwitch, NgSwitchDefault],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
